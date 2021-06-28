d3592dc77fef414a14fce6598aac7b1b4af6aa3b
Rahul Kumar <rahuldbs@gmail.com>
2021-06-28 15:39:42 +0530

    fixed issue in album component

--- src/components/Album.js
+++ src/components/Album.js
@@ -6,7 +6,7 @@ import styles from "./AlbumList.module.css";
 export class Album extends React.Component {
     render() {
         return (
-            <div key={album.id} className={styles.albumCard}>
+            <div key={this.props.key} className={styles.albumCard}>
                 <div className={styles.albumId}>{this.props.albumId}</div>
                 <img src={this.props.thumbnailUrl} alt={"album image"} />
                 <div className={styles.description}>{this.props.title}</div>
